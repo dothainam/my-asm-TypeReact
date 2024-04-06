1b58eea4cc61f0bbd97e9314d0a4d582f65f2520
dothainam <namdtph44461@fpt.edu.vn>
2024-04-06 19:37:02 +0700

    update

--- src/pages/client/pagesClient/Home.tsx
+++ src/pages/client/pagesClient/Home.tsx
@@ -52,6 +52,12 @@ const Home = () => {
                   >
                     Login
                   </Link>
+                  <Link
+                    to='/admin'
+                    className='text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium'
+                  >
+                    Log to admin
+                  </Link>
                 </div>
               </div>
             </div>
